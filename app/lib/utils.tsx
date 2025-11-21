48c4eff20e00dfbea949363979d07aee5d86867e
Evelina <sokolovskayaevi@gmail.com>
2025-11-21 18:38:45 +0300

    add backend for main page

--- app/lib/utils.tsx
+++ app/lib/utils.tsx
@@ -9,92 +9,92 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getMetroStationColor(station: MetroStationEnum): string {
   switch (station) {
-    // Линия 1 — Кировско-Выборгская (красная)
-    case MetroStationEnum.DEVYATKINO:
-    case MetroStationEnum.GRAZHDANSKIY_PROSPEKT:
-    case MetroStationEnum.AKADEMICHSKAYA:
-    case MetroStationEnum.POLITEKHNICHESKAYA:
-    case MetroStationEnum.PLOSHCHAD_MUZHESTVA:
-    case MetroStationEnum.LESNAYA:
-    case MetroStationEnum.VYBORGSKAYA:
-    case MetroStationEnum.PLOSHCHAD_LENINA:
-    case MetroStationEnum.CHERNYSHEVSKAYA:
-    case MetroStationEnum.PLOSHCHAD_VOSSTANIYA:
-    case MetroStationEnum.VLADIMIRSKAYA:
-    case MetroStationEnum.PUSHKINSKAYA:
-    case MetroStationEnum.TEKHNOLOGICHESKIY_INSTITUT_1:
-    case MetroStationEnum.BALTIYSKAYA:
-    case MetroStationEnum.NARVSKAYA:
-    case MetroStationEnum.KIROVSKIY_ZAVOD:
-    case MetroStationEnum.AVTOVO:
-    case MetroStationEnum.LENINSKIY_PROSPEKT:
-    case MetroStationEnum.PROSPEKT_VETERANOV:
+      // Линия 1 — Кировско-Выборгская (красная)
+    case MetroStationEnum.Devyatkino:
+    case MetroStationEnum.GrazhdanskiyProspekt:
+    case MetroStationEnum.Akademichskaya:
+    case MetroStationEnum.Politekhnicheskaya:
+    case MetroStationEnum.PloshchadMuzhestva:
+    case MetroStationEnum.Lesnaya:
+    case MetroStationEnum.Vyborgskaya:
+    case MetroStationEnum.PloshchadLenina:
+    case MetroStationEnum.Chernyshevskaya:
+    case MetroStationEnum.PloshchadVosstaniya:
+    case MetroStationEnum.Vladimirskaya:
+    case MetroStationEnum.Pushkinskaya:
+    case MetroStationEnum.TekhnologicheskiyInstitut1:
+    case MetroStationEnum.Baltiyskaya:
+    case MetroStationEnum.Narvskaya:
+    case MetroStationEnum.KirovskiyZavod:
+    case MetroStationEnum.Avtovo:
+    case MetroStationEnum.LeninskiyProspekt:
+    case MetroStationEnum.ProspektVeteranov:
       return "bg-red-600"
 
-    // Линия 2 — Московско-Петроградская (синяя)
-    case MetroStationEnum.PARNAS:
-    case MetroStationEnum.PROSPEKT_PROSVESHCHENIYA:
-    case MetroStationEnum.OZERKI:
-    case MetroStationEnum.UDELNAYA:
-    case MetroStationEnum.PIONERSKAYA:
-    case MetroStationEnum.CHYORNAYA_RECHKA:
-    case MetroStationEnum.PETROGRADSKAYA:
-    case MetroStationEnum.GORKOVSKAYA:
-    case MetroStationEnum.NEVSKIY_PROSPEKT:
-    case MetroStationEnum.SENNAYA_PLOSHCHAD:
-    case MetroStationEnum.TEKHNOLOGICHESKIY_INSTITUT_2:
-    case MetroStationEnum.FRUNZENSKAYA:
-    case MetroStationEnum.MOSKOVSKIE_VOROTA:
-    case MetroStationEnum.ELEKTROSILA:
-    case MetroStationEnum.PARK_POBEDY:
-    case MetroStationEnum.MOSKOVSKAYA:
-    case MetroStationEnum.ZVEZDNAYA:
-    case MetroStationEnum.KUPCHINO:
+      // Линия 2 — Московско-Петроградская (синяя)
+    case MetroStationEnum.Parnas:
+    case MetroStationEnum.ProspektProsveshcheniya:
+    case MetroStationEnum.Ozerki:
+    case MetroStationEnum.Udelnaya:
+    case MetroStationEnum.Pionerskaya:
+    case MetroStationEnum.ChyornayaRechka:
+    case MetroStationEnum.Petrogradskaya:
+    case MetroStationEnum.Gorkovskaya:
+    case MetroStationEnum.NevskiyProspekt:
+    case MetroStationEnum.SennayaPloshchad:
+    case MetroStationEnum.TekhnologicheskiyInstitut2:
+    case MetroStationEnum.Frunzenskaya:
+    case MetroStationEnum.MoskovskieVorota:
+    case MetroStationEnum.Elektrosila:
+    case MetroStationEnum.ParkPobedy:
+    case MetroStationEnum.Moskovskaya:
+    case MetroStationEnum.Zvezdnaya:
+    case MetroStationEnum.Kupchino:
       return "bg-blue-600"
 
-    // Линия 3 — Невско-Василеостровская (зелёная)
-    case MetroStationEnum.BEGOVAYA:
-    case MetroStationEnum.ZENIT:
-    case MetroStationEnum.PRIMORSKAYA:
-    case MetroStationEnum.VASILEOSTROVSKAYA:
-    case MetroStationEnum.GOSTINY_DVOR:
-    case MetroStationEnum.MAYAKOVSKAYA:
-    case MetroStationEnum.PLOSHCHAD_ALEXANDRA_NEVSKOGO_1:
-    case MetroStationEnum.ELIZAROVSKAYA:
-    case MetroStationEnum.LOMONOSOVSKAYA:
-    case MetroStationEnum.PROLETARSKAYA:
-    case MetroStationEnum.OBUKHOVO:
-    case MetroStationEnum.RYBATSKOYE:
+      // Линия 3 — Невско-Василеостровская (зелёная)
+    case MetroStationEnum.Begovaya:
+    case MetroStationEnum.Zenit:
+    case MetroStationEnum.Primorskaya:
+    case MetroStationEnum.Vasileostrovskaya:
+    case MetroStationEnum.GostinyDvor:
+    case MetroStationEnum.Mayakovskaya:
+    case MetroStationEnum.PloshchadAlexandraNevskogo1:
+    case MetroStationEnum.Elizarovskaya:
+    case MetroStationEnum.Lomonosovskaya:
+    case MetroStationEnum.Proletarskaya:
+    case MetroStationEnum.Obukhovo:
+    case MetroStationEnum.Rybatskoye:
       return "bg-green-600"
 
-    // Линия 4 — Правобережная (оранжевая)
-    case MetroStationEnum.GORNY_INSTITUT:
-    case MetroStationEnum.SPASSKAYA:
-    case MetroStationEnum.DOSTOEVSKAYA:
-    case MetroStationEnum.LIGOVSKIY_PROSPEKT:
-    case MetroStationEnum.PLOSHCHAD_ALEXANDRA_NEVSKOGO_2:
-    case MetroStationEnum.NOVOCHERKASSKAYA:
-    case MetroStationEnum.LADOZHSKAYA:
-    case MetroStationEnum.PROSPEKT_BOLSHEVIKOV:
-    case MetroStationEnum.ULICA_DYBENKO:
+      // Линия 4 — Правобережная (оранжевая)
+    case MetroStationEnum.GornyInstitut:
+    case MetroStationEnum.Spasskaya:
+    case MetroStationEnum.Dostoevskaya:
+    case MetroStationEnum.LigovskiyProspekt:
+    case MetroStationEnum.PloshchadAlexandraNevskogo2:
+    case MetroStationEnum.Novocherkasskaya:
+    case MetroStationEnum.Ladozhskaya:
+    case MetroStationEnum.ProspektBolshevikov:
+    case MetroStationEnum.UlicaDybenko:
       return "bg-yellow-500"
 
-    // Линия 5 — Фрунзенско-Приморская (фиолетовая)
-    case MetroStationEnum.KOMENDANTSKIY_PROSPEKT:
-    case MetroStationEnum.STARAYA_DEREVNYA:
-    case MetroStationEnum.KRESTOVSKIY_OSTROV:
-    case MetroStationEnum.CHKALOVSKAYA:
-    case MetroStationEnum.SPORTIVNAYA:
-    case MetroStationEnum.ADMIRALTEYSKAYA:
-    case MetroStationEnum.SADOVAYA:
-    case MetroStationEnum.ZVENIGORODSKAYA:
-    case MetroStationEnum.OBVODNYY_KANAL:
-    case MetroStationEnum.VOLKOVSKAYA:
-    case MetroStationEnum.BUKHARESTSKAYA:
-    case MetroStationEnum.MEZHDUNARODNAYA:
-    case MetroStationEnum.PROSPEKT_SLAVY:
-    case MetroStationEnum.DUNAISKAYA:
-    case MetroStationEnum.SHUSHARY:
+      // Линия 5 — Фрунзенско-Приморская (фиолетовая)
+    case MetroStationEnum.KomendantskiyProspekt:
+    case MetroStationEnum.StarayaDerevnya:
+    case MetroStationEnum.KrestovskiyOstrov:
+    case MetroStationEnum.Chkalovskaya:
+    case MetroStationEnum.Sportivnaya:
+    case MetroStationEnum.Admiralteyskaya:
+    case MetroStationEnum.Sadovaya:
+    case MetroStationEnum.Zvenigorodskaya:
+    case MetroStationEnum.ObvodnyyKanal:
+    case MetroStationEnum.Volkovskaya:
+    case MetroStationEnum.Bukharestskaya:
+    case MetroStationEnum.Mezhdunarodnaya:
+    case MetroStationEnum.ProspektSlavy:
+    case MetroStationEnum.Dunaiskaya:
+    case MetroStationEnum.Shushary:
       return "bg-purple-600"
 
     default:
@@ -105,88 +105,88 @@ export function getMetroStationColor(station: MetroStationEnum): string {
 export function getMetroStationOptions() {
   const groupMap: Record<string, MetroStationEnum[]> = {
     "Кировско-Выборгская": [
-      MetroStationEnum.DEVYATKINO,
-      MetroStationEnum.GRAZHDANSKIY_PROSPEKT,
-      MetroStationEnum.AKADEMICHSKAYA,
-      MetroStationEnum.POLITEKHNICHESKAYA,
-      MetroStationEnum.PLOSHCHAD_MUZHESTVA,
-      MetroStationEnum.LESNAYA,
-      MetroStationEnum.VYBORGSKAYA,
-      MetroStationEnum.PLOSHCHAD_LENINA,
-      MetroStationEnum.CHERNYSHEVSKAYA,
-      MetroStationEnum.PLOSHCHAD_VOSSTANIYA,
-      MetroStationEnum.VLADIMIRSKAYA,
-      MetroStationEnum.PUSHKINSKAYA,
-      MetroStationEnum.TEKHNOLOGICHESKIY_INSTITUT_1,
-      MetroStationEnum.BALTIYSKAYA,
-      MetroStationEnum.NARVSKAYA,
-      MetroStationEnum.KIROVSKIY_ZAVOD,
-      MetroStationEnum.AVTOVO,
-      MetroStationEnum.LENINSKIY_PROSPEKT,
-      MetroStationEnum.PROSPEKT_VETERANOV,
+      MetroStationEnum.Devyatkino,
+      MetroStationEnum.GrazhdanskiyProspekt,
+      MetroStationEnum.Akademichskaya,
+      MetroStationEnum.Politekhnicheskaya,
+      MetroStationEnum.PloshchadMuzhestva,
+      MetroStationEnum.Lesnaya,
+      MetroStationEnum.Vyborgskaya,
+      MetroStationEnum.PloshchadLenina,
+      MetroStationEnum.Chernyshevskaya,
+      MetroStationEnum.PloshchadVosstaniya,
+      MetroStationEnum.Vladimirskaya,
+      MetroStationEnum.Pushkinskaya,
+      MetroStationEnum.TekhnologicheskiyInstitut1,
+      MetroStationEnum.Baltiyskaya,
+      MetroStationEnum.Narvskaya,
+      MetroStationEnum.KirovskiyZavod,
+      MetroStationEnum.Avtovo,
+      MetroStationEnum.LeninskiyProspekt,
+      MetroStationEnum.ProspektVeteranov
     ],
     "Московско-Петроградская": [
-      MetroStationEnum.PARNAS,
-      MetroStationEnum.PROSPEKT_PROSVESHCHENIYA,
-      MetroStationEnum.OZERKI,
-      MetroStationEnum.UDELNAYA,
-      MetroStationEnum.PIONERSKAYA,
-      MetroStationEnum.CHYORNAYA_RECHKA,
-      MetroStationEnum.PETROGRADSKAYA,
-      MetroStationEnum.GORKOVSKAYA,
-      MetroStationEnum.NEVSKIY_PROSPEKT,
-      MetroStationEnum.SENNAYA_PLOSHCHAD,
-      MetroStationEnum.TEKHNOLOGICHESKIY_INSTITUT_2,
-      MetroStationEnum.FRUNZENSKAYA,
-      MetroStationEnum.MOSKOVSKIE_VOROTA,
-      MetroStationEnum.ELEKTROSILA,
-      MetroStationEnum.PARK_POBEDY,
-      MetroStationEnum.MOSKOVSKAYA,
-      MetroStationEnum.ZVEZDNAYA,
-      MetroStationEnum.KUPCHINO,
+      MetroStationEnum.Parnas,
+      MetroStationEnum.ProspektProsveshcheniya,
+      MetroStationEnum.Ozerki,
+      MetroStationEnum.Udelnaya,
+      MetroStationEnum.Pionerskaya,
+      MetroStationEnum.ChyornayaRechka,
+      MetroStationEnum.Petrogradskaya,
+      MetroStationEnum.Gorkovskaya,
+      MetroStationEnum.NevskiyProspekt,
+      MetroStationEnum.SennayaPloshchad,
+      MetroStationEnum.TekhnologicheskiyInstitut2,
+      MetroStationEnum.Frunzenskaya,
+      MetroStationEnum.MoskovskieVorota,
+      MetroStationEnum.Elektrosila,
+      MetroStationEnum.ParkPobedy,
+      MetroStationEnum.Moskovskaya,
+      MetroStationEnum.Zvezdnaya,
+      MetroStationEnum.Kupchino
     ],
     "Невско-Василеостровская": [
-      MetroStationEnum.BEGOVAYA,
-      MetroStationEnum.ZENIT,
-      MetroStationEnum.PRIMORSKAYA,
-      MetroStationEnum.VASILEOSTROVSKAYA,
-      MetroStationEnum.GOSTINY_DVOR,
-      MetroStationEnum.MAYAKOVSKAYA,
-      MetroStationEnum.PLOSHCHAD_ALEXANDRA_NEVSKOGO_1,
-      MetroStationEnum.ELIZAROVSKAYA,
-      MetroStationEnum.LOMONOSOVSKAYA,
-      MetroStationEnum.PROLETARSKAYA,
-      MetroStationEnum.OBUKHOVO,
-      MetroStationEnum.RYBATSKOYE,
+      MetroStationEnum.Begovaya,
+      MetroStationEnum.Zenit,
+      MetroStationEnum.Primorskaya,
+      MetroStationEnum.Vasileostrovskaya,
+      MetroStationEnum.GostinyDvor,
+      MetroStationEnum.Mayakovskaya,
+      MetroStationEnum.PloshchadAlexandraNevskogo1,
+      MetroStationEnum.Elizarovskaya,
+      MetroStationEnum.Lomonosovskaya,
+      MetroStationEnum.Proletarskaya,
+      MetroStationEnum.Obukhovo,
+      MetroStationEnum.Rybatskoye
     ],
-    Правобережная: [
-      MetroStationEnum.GORNY_INSTITUT,
-      MetroStationEnum.SPASSKAYA,
-      MetroStationEnum.DOSTOEVSKAYA,
-      MetroStationEnum.LIGOVSKIY_PROSPEKT,
-      MetroStationEnum.PLOSHCHAD_ALEXANDRA_NEVSKOGO_2,
-      MetroStationEnum.NOVOCHERKASSKAYA,
-      MetroStationEnum.LADOZHSKAYA,
-      MetroStationEnum.PROSPEKT_BOLSHEVIKOV,
-      MetroStationEnum.ULICA_DYBENKO,
+    "Правобережная": [
+      MetroStationEnum.GornyInstitut,
+      MetroStationEnum.Spasskaya,
+      MetroStationEnum.Dostoevskaya,
+      MetroStationEnum.LigovskiyProspekt,
+      MetroStationEnum.PloshchadAlexandraNevskogo2,
+      MetroStationEnum.Novocherkasskaya,
+      MetroStationEnum.Ladozhskaya,
+      MetroStationEnum.ProspektBolshevikov,
+      MetroStationEnum.UlicaDybenko
     ],
     "Фрунзенско-Приморская": [
-      MetroStationEnum.KOMENDANTSKIY_PROSPEKT,
-      MetroStationEnum.STARAYA_DEREVNYA,
-      MetroStationEnum.KRESTOVSKIY_OSTROV,
-      MetroStationEnum.CHKALOVSKAYA,
-      MetroStationEnum.SPORTIVNAYA,
-      MetroStationEnum.ADMIRALTEYSKAYA,
-      MetroStationEnum.SADOVAYA,
-      MetroStationEnum.ZVENIGORODSKAYA,
-      MetroStationEnum.OBVODNYY_KANAL,
-      MetroStationEnum.VOLKOVSKAYA,
-      MetroStationEnum.BUKHARESTSKAYA,
-      MetroStationEnum.MEZHDUNARODNAYA,
-      MetroStationEnum.PROSPEKT_SLAVY,
-      MetroStationEnum.DUNAISKAYA,
-      MetroStationEnum.SHUSHARY,
-    ],
+      MetroStationEnum.KomendantskiyProspekt,
+      MetroStationEnum.StarayaDerevnya,
+      MetroStationEnum.KrestovskiyOstrov,
+      MetroStationEnum.Chkalovskaya,
+      MetroStationEnum.Sportivnaya,
+      MetroStationEnum.Admiralteyskaya,
+      MetroStationEnum.Sadovaya,
+      MetroStationEnum.Zvenigorodskaya,
+      MetroStationEnum.ObvodnyyKanal,
+      MetroStationEnum.Volkovskaya,
+      MetroStationEnum.Bukharestskaya,
+      MetroStationEnum.Mezhdunarodnaya,
+      MetroStationEnum.ProspektSlavy,
+      MetroStationEnum.Dunaiskaya,
+      MetroStationEnum.Shushary
+    ]
   }
 
   return Object.entries(groupMap).map(([lineName, stations]) => ({
